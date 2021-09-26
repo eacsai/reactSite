153476f144ca942a2e7eda558453c1b92d0c19d5
wangqiwei04 <wangqiwei04@meituan.com>
2021-09-26 22:29:34 +0800

    fixfix

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,5 @@
 const devBaseURL = "https://127.0.0.1:7001/";
-const proBaseURL = "https://innovise.top:7001/";
+const proBaseURL = "http://103.79.78.232:7000/";
 export const BASE_URL = process.env.NODE_ENV === 'development' ? devBaseURL: proBaseURL;
-
 export const TIMEOUT = 5000;
  
