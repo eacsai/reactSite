ea6f67109ff1108ea89141eba3e12f7e6c769070
wangqiwei04 <wangqiwei04@meituan.com>
2021-09-26 21:17:20 +0800

    koa fix

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,5 @@
 const devBaseURL = "http://127.0.0.1:7001/";
-const proBaseURL = "http://127.0.0.1:7001/";
+const proBaseURL = "https://innovise.top:7001/";
 export const BASE_URL = process.env.NODE_ENV === 'development' ? devBaseURL: proBaseURL;
 
 export const TIMEOUT = 5000;
